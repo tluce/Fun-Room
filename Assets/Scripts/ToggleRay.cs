6ef4c926073a4b440167546e2cce346c37185d35
TL <34001240+tluce@users.noreply.github.com>
2022-07-12 16:59:05 +0200

    Teleportation tweaking

---
+++ Assets/Scripts/ToggleRay.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+/// <summary>
+/// Toggle the ray interactor
+/// Should be placed on a ray interactor
+/// </summary>
+[RequireComponent(typeof(XRRayInteractor))]
+public class ToggleRay : MonoBehaviour
+{
+    [Tooltip("The direct interactor that's switched to")]
+    [SerializeField] private XRDirectInteractor directInteractor = null;
+
+    private XRRayInteractor rayInteractor = null;
+
+    private void Awake()
+    {
+        rayInteractor = GetComponent<XRRayInteractor>();
+    }
+
+    public void ActivateRay()
+    {
+        if (!TouchingObject())
+        {
+            SetRayInteractorEnabled(true);
+        }
+    }
+
+    public void DeactivateRay()
+    {
+        SetRayInteractorEnabled(false);
+    }
+
+    private bool TouchingObject()
+    {
+        List<IXRInteractable> targets = new List<IXRInteractable>();
+        directInteractor.GetValidTargets(targets);
+        return (targets.Count > 0);
+    }
+
+    private void SetRayInteractorEnabled(bool value)
+    {
+        rayInteractor.enabled = value;
+    }
+}
